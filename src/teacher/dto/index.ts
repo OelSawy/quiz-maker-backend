291be7ea3d9d271f0ae2633b41ad834ad3b169c2
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2025-08-24 13:37:10 +0300

    refactor: fix naming

--- src/teacher/dto/index.ts
+++ src/teacher/dto/index.ts
@@ -1,6 +1,5 @@
-export * from './answer.dto';
+export * from '../../common/dto/answer.dto';
 export * from './question.dto';
-export * from './quiz-submission.dto';
 export * from './quiz.dto';
 export * from './question-response.dto';
 export * from './quiz-response.dto';
