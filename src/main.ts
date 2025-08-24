1c1a5f2a455fd6dd27d1f5807f0829302aeafe47
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2025-08-24 03:04:39 +0300

    feat: enable type transformation

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,13 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
 }
